2ee5a2155dad797110658ce389b293a76f6a65e0
kusumaesha <kusumaesha25@gmail.com>
2024-11-06 09:40:36 +0530

    nov5

---
+++ main.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main(){
+    int a=14,b=5,c=6;
+}
---
+++ star.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i=1;
+    while(i<=4){
+        printf("*",i);
+        i++;
+    }
+    return 0;
+}
